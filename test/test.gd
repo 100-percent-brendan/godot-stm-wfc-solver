1d9f29216d1caaa28d1324e4fc282dd9410852cb
Brendan T. <brendan@programmerbrendan.com>
2026-03-14 01:50:47 -0300

    Laid out bigger test grid.

--- test/test.gd
+++ test/test.gd
@@ -10,11 +10,13 @@ func _ready() -> void:
 	solver.set_seed(44)
 	solver.set_terrain_tile_set(load("res://test/assets/terrain.tres"))
 	solver.set_debug_mode(true)
-	solver.set_debug_delay(0.2)
+	solver.set_debug_delay(0.03)
+	solver.set_dimensions(15, 10)
 	solver.tile_placed.connect(_on_tile_placed)
 	solver.tile_removed.connect(_on_tile_removed)
 	solver.grid_reset.connect(_on_grid_reset)
-	solver.run()
+	var grid := await solver.run()
+	# TODO: Setup code to transpose solution to a tile map layer
 
 func _on_tile_placed(coords : Vector2i, source_id : int, atlas_coords : Vector2i) -> void:
 	tile_map_layer.set_cell(coords, source_id, atlas_coords)
