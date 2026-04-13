fa02a73fc6c95114e3e4a4c52a7e9e4ca8039143
Brendan T. <brendan@programmerbrendan.com>
2026-04-13 20:53:49 -0300

    Moved clear call, and sped up the inter-map delay slightly in the test script.

--- test/test.gd
+++ test/test.gd
@@ -11,8 +11,6 @@ var solver : TiledWFCSolver ## The solver used for debugging.
 ## Run the solver when the test scene is ready.
 func _ready() -> void:
 	for i in range(100):
-		tile_map_layer.clear()
-		
 		var input1 = load("res://test/assets/input1.tscn").instantiate()
 		var input2 = load("res://test/assets/input2.tscn").instantiate()
 		solver = TiledWFCSolver.new(load("res://test/assets/terrain.tres"), [input1, input2])
@@ -23,10 +21,14 @@ func _ready() -> void:
 		solver.tile_placed.connect(_on_tile_placed)
 		solver.tile_removed.connect(_on_tile_removed)
 		solver.grid_reset.connect(_on_grid_reset)
+		
 		var _grid := await solver.run()
 		
-		# Wait 5 seconds after success
-		await get_tree().create_timer(1.0).timeout
+		# Wait 3 seconds after success
+		await get_tree().create_timer(3.0).timeout
+		
+		# Clear tiles between runs
+		tile_map_layer.clear()
 
 ## When a tile is placed in the solver, place it on the test [TileMapLayer].
 func _on_tile_placed(coords : Vector2i, source_id : int, atlas_coords : Vector2i) -> void:
